781360dfa404dc489353e8adcb970faafa806cd4
Dreaming Tulpa <hey@dreamingtulpa.com>
2022-12-19 15:26:14 +0100

    add webhook_url attr_accessor to Replicate::Client

--- lib/replicate/configurable.rb
+++ lib/replicate/configurable.rb
@@ -2,14 +2,14 @@
 
 module Replicate
   module Configurable
-    attr_accessor :api_token
+    attr_accessor :api_token, :webhook_url
     attr_writer :api_endpoint
 
     class << self
       # List of configurable keys for {Datatrans::Client}
       # @return [Array] of option keys
       def keys
-        @keys ||= %i[api_token api_endpoint]
+        @keys ||= %i[api_token api_endpoint webhook_url]
       end
     end
 
