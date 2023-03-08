b9dbb079781126324c35f8517f78ee545289da0d
Daniel Puglisi <daniel@codegestalt.com>
2023-03-08 15:06:13 +0100

    add upload_zip helper method

--- lib/replicate/client/upload.rb
+++ lib/replicate/client/upload.rb
@@ -4,10 +4,18 @@ module Replicate
   class Client
     # Methods for the Prediction API
     module Upload
+      # Create upload object and upload zip file
+      def upload_zip(zip_path)
+        filename = zip_path.split('/')[-1]
+        upload = create_upload(filename)
+        upload.attach(zip_path)
+        upload
+      end
+
       # Create an upload
       # @see https://replicate.com/blog/dreambooth-api
-      def create_upload
-        response = dreambooth_endpoint.post("upload/data.zip")
+      def create_upload(filename = 'data.zip')
+        response = dreambooth_endpoint.post("upload/#{filename}")
         Replicate::Record::Upload.new(self, response)
       end
 
